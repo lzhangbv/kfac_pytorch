fixed kl clip and running average bugs and validated training performance of kfac eigen-dp

examples/pytorch_cifar10_resnet.py
@@ -36,7 +36,8 @@ import kfac.backend as backend #don't use a `from` import
 import horovod.torch as hvd
 import torch.distributed as dist
 
-SPEED = True
+#SPEED = True
+SPEED = False
 
 def initialize():
     # Training Parameters

examples/pytorch_multi30k_transformer.py
@@ -27,7 +27,8 @@ formatter = logging.Formatter('%(asctime)s [%(filename)s:%(lineno)d] %(levelname
 strhdlr.setFormatter(formatter)
 logger.addHandler(strhdlr) 
 
-SPEED = True
+#SPEED = True
+SPEED = False
 
 import torch
 import torch.backends.cudnn as cudnn

examples/pytorch_squad_bert.py
@@ -37,7 +37,8 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-SPEED = True
+#SPEED = True
+SPEED = False
 
 # for bert
 from transformers import (