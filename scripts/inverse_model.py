86c6b94dc00c2d3e2c5068acacf2c5e0d069ad20
lzhangbv <lzhangbv@connect.ust.hk>
2022-08-26 09:56:03 +0800

    add: time comparison between inverse and eigen

--- scripts/inverse_model.py
+++ scripts/inverse_model.py
@@ -2,7 +2,15 @@ import math
 import torch
 import numpy as np
 import time
-import torchsso
+
+
+def mat_inv(x):
+    u = torch.linalg.cholesky(x)
+    return torch.cholesky_inverse(u)
+
+def mat_eig(x):
+    eigen_val, eigen_vec = torch.linalg.eigh(x)
+    return eigen_val, eigen_vec.contiguous()
 
 dims = np.array(range(1, 129)) * 64
 L = 10
@@ -13,12 +21,18 @@ module_shape_G = [128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 128, 12
 
 #for dim in module_shape_A:
 for dim in dims:
-    tensor = torch.rand((dim, dim)).cuda()
-    torchsso.utils.inv(tensor)
+    #tensor = torch.rand((dim, dim)).cuda()
+    tensor = torch.rand((dim, 128)).cuda()
+    tensor = tensor @ tensor.T
+    tensor.add_(torch.diag(tensor.new(dim).fill_(0.03)))
+    
+    #mat_inv(tensor)
+    mat_eig(tensor)
 
     stime = time.time()
     for i in range(L):
-        torchsso.utils.inv(tensor)
+        #mat_inv(tensor)
+        mat_eig(tensor)
     ttime = time.time() - stime
     inv_time.append(ttime / L)
 
